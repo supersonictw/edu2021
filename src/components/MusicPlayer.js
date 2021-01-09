be2667d0bad0b685785e25978d58b66e0ad18065
SuperSonic <supersonic@livemail.tw>
2021-01-09 23:31:01 +0800

    Update MID soundfont settings

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -18,14 +18,7 @@ MusicPlayer.prototype = {
         this.music.tracks.forEach((track) => {
             if(!this.playing) return;
             const now = Tone.now() + 0.5;
-            const synth = new Tone.PolySynth(Tone.Synth, {
-                envelope: {
-                    attack: 0.02,
-                    decay: 0.1,
-                    sustain: 0.3,
-                    release: 1,
-                },
-            }).toDestination();
+            const synth = new Tone.PolySynth(Tone.Synth).toDestination();
             track.notes.forEach((note) => {
                 synth.triggerAttackRelease(
                     note.name,
