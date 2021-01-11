46e81028cdf773c70274c17eba78bfe0000c961b
SuperSonic <supersonic@livemail.tw>
2021-01-11 15:27:42 +0000

    Add Google Analytics

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import App from "./App.vue";
 // Configure Plugin Automatically
 import Store from "./data/store.js";
 
+// The snippet is only used for analyzing the visitor with Google Analytics
+import VueGtag from "vue-gtag";
+Vue.use(VueGtag, { config: { id: "G-FDNELQW7T0" } }, Router);
+
+// Vue Settings
 Vue.config.productionTip = false
 
 // Create Vue Application
