e051bee6eeffcde27a66c0ad5ee864932b108963
Shiranui <supersonic@livemail.tw>
2024-01-04 20:56:50 +0800

    Update for new domain

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,8 @@ module.exports = {
     },
     devServer: {
         disableHostCheck: true
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/edu2021/'
+        : '/'
 };
